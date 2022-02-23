abd8808397ac213effa67d938fd5648c0d137cf4
Adam Jonas <adam.jonas@ppb.com>
2022-02-23 09:02:41 +0000

    WIP - Refactoring

--- java-impl/src/main/java/aj/countdown/domain/Calculation.java
+++ java-impl/src/main/java/aj/countdown/domain/Calculation.java
@@ -3,56 +3,59 @@ package aj.countdown.domain;
 import aj.countdown.generator.Operator;
 
 public class Calculation {
-    private final Operator operator;
-    private final int result;
-    private Calculation calculationX;
-    private Calculation calculationY;
-    private int x;
-    private int y;
 
     private StringBuilder solution;
 
+    private int result;
+
     public Calculation(Operator operator, int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.operator = operator;
         this.result = operator.apply(x, y);
         this.solution = new StringBuilder("(")
-                .append(x).append(operator).append(y).append(")");
+                .append(x)
+                .append(operator)
+                .append(y)
+                .append(")");
     }
 
     public Calculation(Operator operator, Calculation calculationX, Calculation calculationY) {
-        this.operator = operator;
-        this.calculationX = calculationX;
-        this.calculationY = calculationY;
         this.result = operator.apply(calculationX.result, calculationY.result);
         this.solution = new StringBuilder("(")
-                .append(calculationX).append(operator).append(calculationY).append(")");
+                .append(calculationX)
+                .append(operator)
+                .append(calculationY)
+                .append(")");
     }
 
     public Calculation(Operator operator, int x, Calculation calculationY) {
-        this.operator = operator;
-        this.x = x;
-        this.calculationY = calculationY;
         this.result = operator.apply(x, calculationY.result);
         this.solution = new StringBuilder("(")
-                .append(x).append(operator).append(calculationY).append(")");
+                .append(x)
+                .append(operator)
+                .append(calculationY)
+                .append(")");
     }
 
     public Calculation(Operator operator, Calculation calculationX, int y) {
-        this.operator = operator;
-        this.calculationX = calculationX;
-        this.y = y;
         this.result = operator.apply(calculationX.result, y);
         this.solution = new StringBuilder("(")
-                .append(calculationX).append(operator).append(y).append(")");
+                .append(calculationX)
+                .append(operator)
+                .append(y)
+                .append(")");
     }
 
     public Calculation(int x) {
-        this.x = x;
         this.result = x;
-        this.solution = new StringBuilder(x);
-        this.operator = null;
+        this.solution = new StringBuilder(String.valueOf(result));
+    }
+
+    public void calculate(Operator operator, Calculation calculation) {
+        this.result = operator.apply(result, calculation.result);
+        this.solution = new StringBuilder("(")
+                .append(this)
+                .append(operator)
+                .append(calculation)
+                .append(")");
     }
 
     public int getResult() {
@@ -60,7 +63,7 @@ public class Calculation {
     }
 
     public String getSolution() {
-        return toString();
+        return solution.toString();
     }
 
     @Override
--- java-impl/src/main/java/aj/countdown/domain/Calculator.java
+++ java-impl/src/main/java/aj/countdown/domain/Calculator.java
@@ -6,6 +6,7 @@ import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static aj.countdown.generator.Operator.DIVIDE;
 
@@ -13,80 +14,36 @@ public class Calculator {
     private static final XoRoShiRo128PlusRandom RANDOM = new XoRoShiRo128PlusRandom();
     private static final List<Operator> OPERATORS = Arrays.asList(Operator.values());
 
-    private final StringBuilder solution = new StringBuilder();
-
     public Calculation calculate(List<Integer> numbers) {
-        List<Calculation> calculations = calculateInts(numbers);
-        return calculations.size() == 1 ? calculations.get(0) :
-                calculateCalcs(calculations);
+        List<Calculation> calculations = numbers.stream().map(Calculation::new)
+                .collect(Collectors.toList());
+        return calculateTarget(calculations);
     }
 
-    private List<Calculation> calculateInts(List<Integer> numbers) {
-        List<Calculation> calculations = new ArrayList<>();
-        for (int i = 0; i < numbers.size(); i++) {
-            Calculation calculation = null;
-            int x = numbers.get(i);
-            if (i == 0 || (i < numbers.size() - 1 && RANDOM.nextBoolean())) {
-                int y = numbers.get(++i);
-                while (calculation == null) {
-                    calculation = calculate(x, y);
-                }
-            } else {
-                Calculation calculationY = calculations.get(RANDOM.nextInt(calculations.size()));
-                calculations.remove(calculationY);
-                while (calculation == null) {
-                    calculation = calculate(x, calculationY);
-                }
-            }
-            calculations.add(calculation);
+    private Calculation calculateTarget(List<Calculation> inputs) {
+        List<Calculation> results = new ArrayList<>();
+        for (int i = 0; i < inputs.size(); i++) {
+            Calculation x = inputs.get(i);
+            Calculation y = (i == 0 || (i < inputs.size() - 1 && RANDOM.nextBoolean())) ?
+                    inputs.get(++i) : results.get(RANDOM.nextInt(results.size()));
+            results.add(calculate(x, y));
         }
-        return calculations;
-    }
-
-    private Calculation calculateCalcs(List<Calculation> calculations) {
-        Calculation calculation = null;
-        Calculation x = calculations.get(0);
-        Calculation y = calculations.get(1);
-        while (calculation == null) {
-            calculation = calculate(x, y);
-        }
-        if (calculations.size() == 2) return calculation;
-        y = calculations.get(2);
-        Calculation newCalculation = null;
-        while (newCalculation == null) {
-            newCalculation = calculate(calculation, y);
-        }
-        return newCalculation;
-    }
-
-    private Calculation calculate(int x, int y) {
-        Operator operator = getOperator();
-        if (operator.apply(x, y) == 0)  return null;
-        if (operator.equals(DIVIDE)) {
-            if (x % y == 0) return new Calculation(operator, x, y);
-            if (y % x == 0) return new Calculation(operator, y, x);
-            return null;
-        }
-        return new Calculation(operator, x, y);
-    }
-
-    private Calculation calculate(int x, Calculation y) {
-        Operator operator = getOperator();
-        if (operator.apply(x, y.getResult()) == 0)  return null;
-        if (operator.equals(DIVIDE)) {
-            if (x % y.getResult() == 0) return new Calculation(operator, x, y);
-            if (y.getResult() % x == 0) return new Calculation(operator, y, x);
-            return null;
-        }
-        return new Calculation(operator, x, y);
+        return results.size() == 1 ? results.get(0) : calculateTarget(results);
     }
 
     private Calculation calculate(Calculation x, Calculation y) {
-        Operator operator = getOperator();
+        Calculation result = null;
+        while (result == null) {
+            result = getResult(getOperator(), x, y);
+        }
+        return result;
+    }
+
+    private Calculation getResult(Operator operator, Calculation x, Calculation y) {
         if (operator.apply(x.getResult(), y.getResult()) == 0)  return null;
         if (operator.equals(DIVIDE)) {
-            if (x.getResult() % y.getResult() == 0) return new Calculation(operator, x, y);
-            if (y.getResult() % x.getResult() == 0) return new Calculation(operator, y, x);
+            if (x.getResult() % y.getResult() == 0) x.calculate(operator, y);
+            if (y.getResult() % x.getResult() == 0) y.calculate(operator, y);
             return null;
         }
         return new Calculation(operator, x, y);
@@ -96,3 +53,44 @@ public class Calculator {
         return OPERATORS.get(RANDOM.nextInt(OPERATORS.size()));
     }
 }
+
+
+
+//
+//    private Calculation calculateRemaining(List<Calculation> calculations) {
+//        Calculation calculation = null;
+//        Calculation x = calculations.get(0);
+//        Calculation y = calculations.get(1);
+//        while (calculation == null) {
+//            calculation = calculate(x, y);
+//        }
+//        if (calculations.size() == 2) return calculation;
+//        y = calculations.get(2);
+//        Calculation newCalculation = null;
+//        while (newCalculation == null) {
+//            newCalculation = calculate(calculation, y);
+//        }
+//        return newCalculation;
+//    }
+//
+//    private Calculation calculate(int x, int y) {
+//        Operator operator = getOperator();
+//        if (operator.apply(x, y) == 0)  return null;
+//        if (operator.equals(DIVIDE)) {
+//            if (x % y == 0) return new Calculation(operator, x, y);
+//            if (y % x == 0) return new Calculation(operator, y, x);
+//            return null;
+//        }
+//        return new Calculation(operator, x, y);
+//    }
+//
+//    private Calculation calculate(int x, Calculation y) {
+//        Operator operator = getOperator();
+//        if (operator.apply(x, y.getResult()) == 0)  return null;
+//        if (operator.equals(DIVIDE)) {
+//            if (x % y.getResult() == 0) return new Calculation(operator, x, y);
+//            if (y.getResult() % x == 0) return new Calculation(operator, y, x);
+//            return null;
+//        }
+//        return new Calculation(operator, x, y);
+//    }
