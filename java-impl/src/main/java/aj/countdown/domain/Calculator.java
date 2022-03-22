34b8bb424d06d23185b418c4a7b3381b46ec68b3
Adam Jonas <adam.jonas@ppb.com>
2022-03-22 07:22:40 +0000

    Add solve mode

--- java-impl/src/main/java/aj/countdown/domain/Calculator.java
+++ java-impl/src/main/java/aj/countdown/domain/Calculator.java
@@ -1,6 +1,7 @@
 package aj.countdown.domain;
 
 import aj.countdown.generator.Operator;
+import aj.countdown.solver.Solver;
 import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 
 import java.util.ArrayList;
@@ -8,6 +9,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import static aj.countdown.generator.Operator.DIVIDE;
+import static aj.countdown.solver.Solver.SolveMode.RUNNING;
+import static java.util.Collections.shuffle;
 
 public class Calculator {
     private static final XoRoShiRo128PlusRandom RANDOM = new XoRoShiRo128PlusRandom();
@@ -21,23 +24,33 @@ public class Calculator {
         return calculateTarget(calculations);
     }
 
-    public Calculation calculateSolution(List<Integer> numbers) {
+    public Calculation calculateSolution(List<Integer> numbers, Solver.SolveMode mode) {
         int target = numbers.remove(numbers.size() - 1);
         List<Calculation> calculations = numbers.stream()
                 .map(Calculation::new)
                 .toList();
 
+        if (mode.equals(RUNNING)) return calculateRunning(calculations, target);
         return calculateSolution(calculations, target);
+    }
+
+    private Calculation calculateRunning(List<Calculation> calculations, int target) {
+        calculations = new ArrayList<>(calculations);
+        shuffle(calculations);
+        var x = calculations.get(0);
+        for (int i = 1; i < calculations.size(); i++) {
+            if (x.getResult() == target) return x;
+            x = calculate(x, calculations.get(i));
+        }
+        return x;
     }
 
     private Calculation calculateTarget(List<Calculation> inputs) {
         List<Calculation> results = new ArrayList<>();
         for (int i = 0; i < inputs.size(); i++) {
             Calculation x = inputs.get(i);
-            Calculation y =
-                    (i == 0 || (i < inputs.size() - 1 && RANDOM.nextBoolean())) ?
+            Calculation y = (i == 0 || (i < inputs.size() - 1 && RANDOM.nextBoolean())) ?
                     inputs.get(++i) : results.remove(RANDOM.nextInt(results.size()));
-
             results.add(calculate(x, y));
         }
         return results.size() == 1 ? results.get(0) : calculateTarget(results);
@@ -47,9 +60,8 @@ public class Calculator {
         List<Calculation> results = new ArrayList<>();
         for (int i = 0; i < inputs.size(); i++) {
             Calculation x = inputs.get(i);
-            Calculation y =
-                    (i == 0 || (i < inputs.size() - 1 && RANDOM.nextBoolean())) ?
-                            inputs.get(++i) : results.remove(RANDOM.nextInt(results.size()));
+            Calculation y = (i == 0 || (i < inputs.size() - 1 && RANDOM.nextBoolean())) ?
+                    inputs.get(++i) : results.remove(RANDOM.nextInt(results.size()));
             Calculation result = calculate(x, y);
             if (result.getResult() == target) return result;
             results.add(calculate(x, y));
--- java-impl/src/main/java/aj/countdown/runner/Main.java
+++ java-impl/src/main/java/aj/countdown/runner/Main.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import static aj.countdown.generator.Filter.NOT_FIVE;
 import static aj.countdown.generator.Filter.ODD;
+import static aj.countdown.solver.Solver.SolveMode.RUNNING;
 
 public class Main {
     private static final Logger LOG = new Logger();
@@ -16,22 +17,24 @@ public class Main {
     private static final Generator GENERATOR = new Generator(CALCULATOR, 5);
     private static final Solver SOLVER = new Solver(CALCULATOR);
 
-    private static final int NUMBER_OF_LARGE = 1;
+    private static final int NUMBER_OF_LARGE = 2;
     private static final int RUNS = 10;
     private static final int WARM_UPS = 10;
 
     private static final List<List<Integer>> testInputs = List.of(
             List.of(50, 25, 75, 2, 1, 100, 199),
             List.of(6, 100, 4, 6, 8, 2, 752),
+            List.of(25, 50, 75, 100, 3, 6, 952),
             List.of(75, 5, 25, 6, 50, 100, 426)
     );
 
     static {
+        SOLVER.setMode(RUNNING);
+        GENERATOR.addFilter(ODD).addFilter(NOT_FIVE);
         warmUp();
     }
 
     public static void main(String[] args) {
-        GENERATOR.addFilter(ODD).addFilter(NOT_FIVE);
         runSolver();
     }
 
